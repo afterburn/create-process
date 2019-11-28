b5837643ff1e4f837527ff0ac89807ad3a78c2de
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2019-11-28 19:28:35 +0100

    Release 0.0.4

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # create-process
-Small utility function to help your create and manage node processes more easily.
-
 ![enter image description here](https://img.shields.io/npm/dt/create-process)
 
-## Installation
-  npm install --save create-process
+Small utility library to help your create and manage node processes more easily.
+(work in progress)
 
+## Installation
+```shell
+npm install --save create-process
+```
+
+## Usage
+```javascript
+const createProcess = require('createProcess')
+
+// Simple usage:
+const process = createProcess('npm run build')
+
+// Advanced usage:
+const process = createProcess({
+  name: 'webpack',
+  cmd: 'npm',
+  args: ['run', 'build'],
+  opts: {
+    cwd: path.join(__dirname, '..')
+  }
+})
+```
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const spawn = require('cross-spawn')
 
+function parseCommand (str) {
+  const config = {}
+  const strData = str.split(' ')
+  config.cmd = strData.shift()
+  config.args = strData
+  config.opts = { cwd: process.cwd() }
+  return config
+}
+
 function createProcess (config) {
+  if (typeof config === 'string') {
+    config = parseCommand(config)
+  }
+
   const name = config.name
   const tag = `[${name}]`
   let { cmd, args, opts } = config
@@ -23,7 +36,7 @@ function createProcess (config) {
     p.stdout.on('data', (data) => console.log(data.toString().trim()))
     p.stdout.on('error', (err) => console.log(err.toString().trim()))
     p.on('error', (err) => console.log(err))
-    p.on('close', (c) => console.log(`process '${name}' exited with code: ${c}`))
+    p.on('close', (c) => console.log(`process exited with code: ${c}`))
   }
 
   return p
